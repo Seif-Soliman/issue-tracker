0bbc5e48304151b2f07b3ca5adcbc86cab51f2e5
Seif-Soliman <seif.soliman@giza.com>
2024-01-23 18:12:36 +0200

    disable ssr

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { Button, Callout, TextField, Text } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
 import axios from "axios";
@@ -11,6 +10,11 @@ import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
+import dynamic from "next/dynamic";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
