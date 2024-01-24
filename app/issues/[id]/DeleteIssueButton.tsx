89a6ac7bb95259bf0d3500eaa31194186f637649
Seif-Soliman <seif.soliman@giza.com>
2024-01-24 16:15:22 +0200

    Delete confirmation box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,16 +1,33 @@
-import { Button } from "@radix-ui/themes";
-import Link from "next/link";
-import React from "react";
+"use client";
 
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 interface Props {
   issueId: number;
 }
 
 const DeleteIssueButton = ({ issueId }: Props) => {
   return (
-    <Button color="red">
-      <Link href={`/issues/${issueId}/delete`}>Delete Issue</Link>
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue?
+        </AlertDialog.Description>
+        <Flex mt="4" gap="3">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Delete Issue</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
