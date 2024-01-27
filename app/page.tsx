aff6b12b1790d72a829ca11236de1cf58cf3884c
Seif-Soliman <seif.soliman@giza.com>
2024-01-27 16:27:19 +0200

    Issue Summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 import LatestIssue from "./LatestIssue";
-import Pagination from "./components/Pagination";
 
-export default function Home() {
-  return <LatestIssue />;
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+  return (
+    <>
+      <LatestIssue />
+      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+    </>
+  );
 }
